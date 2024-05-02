cfbe4c40b48f22a2eaff55015f642e7bdba619d6
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-05-02 16:45:59 -0600

    cambios en el codigo del desafio

--- desafio-strings.py
+++ desafio-strings.py
@@ -3,4 +3,4 @@
 radio_circulo = float(input("Ingresa el valor del radio del circulo: "))
 area_circulo = (radio_circulo ** 2) * 3.14159
 
-print(f"El area del circulo que tiene un radio de: {radio_circulo} es: {area_circulo}")
+print(f"El area del circulo es: {area_circulo}")
