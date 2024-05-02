47d901efea13ea76e0d9c65d62470332a08852bf
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-05-02 15:44:25 -0600

    Agregue comentarios y cambios de nombres en variables

--- colecciones.py
+++ colecciones.py
@@ -1,8 +1,8 @@
 # Estrellas mas cercanas a la Tierra.
-estrella_1 = "Sol"
-estrella_2 = "Alfa Centauri"
-estrella_3 = "Estrella de Barnard"
-estrella_4 = "Luhman 16"
+# estrella_1 = "Sol"
+# estrella_2 = "Alfa Centauri"
+# estrella_3 = "Estrella de Barnard"
+# estrella_4 = "Luhman 16"
 
 # Conevrtirla en una lista e imprimir la 4ta estrella.
 estrellas = ["Sol", "Alfa Centauri", "Estrella de Barnard", "Luhman 16"]
@@ -11,14 +11,14 @@ print(estrellas[3])
 
 
 # Montañas más altas de cada placa tectónica.
-africana = "Kilimanjaro"
-antartica = "Macizo vinson"
-indoaustraliana = "Monte jaya"
-euroasiatica = "Everest"
-norteamericana = "Monte denali"
-pacifico = "Volcan mauna kea"
-suramericana = "Aconcagua"
+# africana = "Kilimanjaro"
+# antartica = "Macizo vinson"
+# indoaustraliana = "Monte jaya"
+# euroasiatica = "Everest"
+# norteamericana = "Monte denali"
+# pacifico = "Volcan mauna kea"
+# suramericana = "Aconcagua"
 
 #Convertirlas a un diccionario e imprimir la montaña mas grande en la placa del pacifico.
-montanas = {"africana":"Kilimanjaro", "antartica":"Macizo vinson", "indoaustraliana":"Monte jaya", "euroasiatica":"Everest", "norteamericana":"Monte denali", "pacifico":"Volcan mauna kea", "suramericana":"Aconcagua"}
-print(montanas["pacifico"])
+montanas_mas_altas = {"africana":"Kilimanjaro", "antartica":"Macizo vinson", "indoaustraliana":"Monte jaya", "euroasiatica":"Everest", "norteamericana":"Monte denali", "pacifico":"Volcan mauna kea", "suramericana":"Aconcagua"}
+print(montanas_mas_altas["pacifico"])
